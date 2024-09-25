40248afae6972f6a1c6567630e58b0d5054ca616
Alok <waynerooney0089@gmail.com>
2024-09-25 09:36:38 +0530

    gameWithIntegers and jaggedSwaps added

---
+++ gameWithIntegers.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define fio                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll = long long;
+using ld = long double;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    int ans;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        // int cnt = 10;
+        // while (cnt--)
+            if ((n - 1) % 3 == 0 || (n + 1) % 3 == 0)
+            {
+                cout<<"First"<<"\n";
+            }
+            else{
+                cout<<"Second"<<"\n";
+            }
+        // if(cnt>0&& )
+    }
+    return 0;
+}
